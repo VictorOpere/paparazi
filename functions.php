844a949f8bbb95237208cfdff459d44c60add119
Victor Opere <victoropere@gmail.com>
2020-07-02 14:42:48 +0300

    just finished adding css and js to our theme

--- functions.php
+++ functions.php
@@ -11,8 +11,10 @@
 
  /***
   * Table Of Contents:
-  *Theme Support
-  * 
+  * Theme Support
+  * Register Styles
+  * Register Scripts
+  *
   */
 
 
@@ -100,16 +102,63 @@
 
     );
 
-
-
     
-
-
    }
 
    add_action( 'init', 'paparazi_theme_support');
 
 
+
+   function paparazi_theme_scripts()
+   {
+       # code...
+    //    lets add our css to our theme
+
+    wp_enqueue_style( 'mypoppins', 'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'mylora', 'https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myabril', 'https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap', array(), '1.0.0', 'all' );
+
+
+    wp_enqueue_style( 'myopenionic', get_template_directory_uri().'/css/open-iconic-bootstrap.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myanimate', get_template_directory_uri().'/css/animate.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myowl', get_template_directory_uri().'/css/owl.carousel.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'mythemeowl', get_template_directory_uri().'/css/owl.theme.default.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'mymagnific', get_template_directory_uri().'/css/magnific-popup.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myaos', get_template_directory_uri().'/css/aos.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myioni', get_template_directory_uri().'/css/ionicons.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'mybtdtpckr', get_template_directory_uri().'/css/bootstrap-datepicker.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myjqtmpckr', get_template_directory_uri().'/css/jquery.timepicker.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myfltcon', get_template_directory_uri().'/css/flaticon.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myicomoon', get_template_directory_uri().'/css/icomoon.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'mybase', get_template_directory_uri().'/css/style.css', array(), '1.0.0', 'all' );
+  
+
+
+    // lets add the js to our theme
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'mypopper', get_template_directory_uri().'/js/popper.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'mybtjs', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myjqeas', get_template_directory_uri().'/js/jquery.easing.1.3.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myjqway', get_template_directory_uri().'/js/jquery.waypoints.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myjqstella', get_template_directory_uri().'/js/jquery.stellar.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myowljs', get_template_directory_uri().'/js/owl.carousel.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myjqmagnific', get_template_directory_uri().'/js/jquery.magnific-popup.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myaosjs', get_template_directory_uri().'/js/aos.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myjqanimate', get_template_directory_uri().'/js/jquery.animateNumber.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myscrollmax', get_template_directory_uri().'/js/scrollax.min.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'mymap', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'myggmap', get_template_directory_uri().'/js/google-map.js', array('jquery'), '1.0.0', True);
+    wp_enqueue_script( 'mymain', get_template_directory_uri().'/js/main.js', array('jquery'), '1.0.0', True);
+
+
+
+
+
+
+
+   }
+
+   add_action( 'wp_enqueue_scripts', 'paparazi_theme_scripts');
 
 
 
---
+++ js/main.js
@@ -0,0 +1,332 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./scss/style.scss":
+/*!*************************!*\
+  !*** ./scss/style.scss ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+AOS.init({
+  duration: 800,
+  easing: 'slide'
+});
+
+(function ($) {
+  "use strict";
+
+  $(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+  var fullHeight = function fullHeight() {
+    $('.js-fullheight').css('height', $(window).height());
+    $(window).resize(function () {
+      $('.js-fullheight').css('height', $(window).height());
+    });
+  };
+
+  fullHeight(); // loader
+
+  var loader = function loader() {
+    setTimeout(function () {
+      if ($('#ftco-loader').length > 0) {
+        $('#ftco-loader').removeClass('show');
+      }
+    }, 1);
+  };
+
+  loader(); // Scrollax
+
+  $.Scrollax();
+
+  var burgerMenu = function burgerMenu() {
+    $('.js-colorlib-nav-toggle').on('click', function (event) {
+      event.preventDefault();
+      var $this = $(this);
+
+      if ($('body').hasClass('offcanvas')) {
+        $this.removeClass('active');
+        $('body').removeClass('offcanvas');
+      } else {
+        $this.addClass('active');
+        $('body').addClass('offcanvas');
+      }
+    });
+  };
+
+  burgerMenu(); // Click outside of offcanvass
+
+  var mobileMenuOutsideClick = function mobileMenuOutsideClick() {
+    $(document).click(function (e) {
+      var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('offcanvas')) {
+          $('body').removeClass('offcanvas');
+          $('.js-colorlib-nav-toggle').removeClass('active');
+        }
+      }
+    });
+    $(window).scroll(function () {
+      if ($('body').hasClass('offcanvas')) {
+        $('body').removeClass('offcanvas');
+        $('.js-colorlib-nav-toggle').removeClass('active');
+      }
+    });
+  };
+
+  mobileMenuOutsideClick();
+
+  var carousel = function carousel() {
+    $('.home-slider').owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      nav: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    });
+    $('.author-slider').owlCarousel({
+      autoplay: true,
+      loop: true,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: true,
+      dots: true,
+      navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    });
+  };
+
+  carousel();
+
+  var contentWayPoint = function contentWayPoint() {
+    var i = 0;
+    $('.ftco-animate').waypoint(function (direction) {
+      if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+        i++;
+        $(this.element).addClass('item-animate');
+        setTimeout(function () {
+          $('body .ftco-animate.item-animate').each(function (k) {
+            var el = $(this);
+            setTimeout(function () {
+              var effect = el.data('animate-effect');
+
+              if (effect === 'fadeIn') {
+                el.addClass('fadeIn ftco-animated');
+              } else if (effect === 'fadeInLeft') {
+                el.addClass('fadeInLeft ftco-animated');
+              } else if (effect === 'fadeInRight') {
+                el.addClass('fadeInRight ftco-animated');
+              } else {
+                el.addClass('fadeInUp ftco-animated');
+              }
+
+              el.removeClass('item-animate');
+            }, k * 50, 'easeInOutExpo');
+          });
+        }, 100);
+      }
+    }, {
+      offset: '95%'
+    });
+  };
+
+  contentWayPoint();
+
+  var counter = function counter() {
+    $('#section-counter').waypoint(function (direction) {
+      if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+        var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
+        $('.number').each(function () {
+          var $this = $(this),
+              num = $this.data('number');
+          console.log(num);
+          $this.animateNumber({
+            number: num,
+            numberStep: comma_separator_number_step
+          }, 7000);
+        });
+      }
+    }, {
+      offset: '95%'
+    });
+  };
+
+  counter(); // magnific popup
+
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    // mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+
+    }
+  });
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+})(jQuery);
+
+/***/ }),
+
+/***/ 0:
+/*!*********************************************!*\
+  !*** multi ./src/main.js ./scss/style.scss ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! C:\xampp3\htdocs\ecommerce\wp-content\themes\paparazi\src\main.js */"./src/main.js");
+module.exports = __webpack_require__(/*! C:\xampp3\htdocs\ecommerce\wp-content\themes\paparazi\scss\style.scss */"./scss/style.scss");
+
+
+/***/ })
+
+/******/ });
